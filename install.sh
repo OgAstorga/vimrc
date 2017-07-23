cb9bf37748f8e2f071edef85569d78cc6b99b62e
Og Astorga <og@getfleety.com>
2017-07-23 11:31:24 -0500

    Use vundle as package manager

--- install.sh
+++ install.sh
@@ -3,6 +3,13 @@ set -e
 
 # Delete prev vim conf
 rm -f ~/.vimrc
+rm -rf ~/.vim/
+
+# Build folder structure
+mkdir -p ~/.vim/bundle
+
+# Install Vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 #  .vimrc
 ln -s ~/.vim_runtime/vimrcs/core.vim ~/.vimrc
